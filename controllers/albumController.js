d21963ff1bd90fe369ebbd1bf8373da585d9e753
Robert Palmer <r91palmer@gmail.com>
2020-09-16 12:41:57 -0500

    added album controller routes

---
+++ controllers/albumController.js
@@ -0,0 +1,41 @@
+const album = require('../models/album');
+
+// Gallery of Photos
+exports.photo_gallery = (req, res) => {
+    res.send('NOT IMPLEMENTED: Photo Gallery');
+};
+
+// Display detail page of specific Album
+exports.album_detail = (req, res) => {
+    res.send(`NOT IMPLEMENTED: album detail: ${req.params.id}`);
+};
+
+// Display Album Create Form - GET
+exports.album_create_get = (req, res) => {
+    res.send(`NOT IMPLEMENTED: album create GET`);
+};
+
+// Handle Album Create - POST
+exports.album_create_post = (req, res) => {
+    res.send(`NOT IMPLEMENTED: album create POST`);
+};
+
+// Display Album Delete - GET
+exports.album_delete_get = (req, res) => {
+    res.send('NOT IMPLEMENTED: album delete GET');
+};
+
+// Handle Album Delete - POST
+exports.album_delete_post = (req, res) => {
+    res.send('NOT IMPLEMENTED: album delete POST');
+};
+
+// Display Album Update - GET
+exports.album_update_get = (req, res) => {
+    res.send('NOT IMPLEMENTED: album update GET');
+};
+
+// Handle Album Update - POST
+exports.album_update_post = (req, res) => {
+    res.send('NOT IMPLEMENTED: album update POST');
+};
