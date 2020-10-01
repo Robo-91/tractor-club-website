bbd542d93c3d5ff82d5ff6a5efa39cb8df56ebb5
Robert Palmer <r91palmer@gmail.com>
2020-09-30 22:09:09 -0500

    modified photo property from album model

--- models/album.js
+++ models/album.js
@@ -6,7 +6,7 @@ const AlbumSchema = new Schema(
     {
         event_name: { type: String, required: true },
         date_submitted: { type: Date, required: true },
-        photo: { type: Schema.Types.ObjectId, ref: 'Photo', required: true }
+        photo: { data: Buffer, contentType: String }
     }
 );
 
