7bdb1e7d8cb672e9809e5ef10ab97cc08b6f4338
Robert Palmer <r91palmer@gmail.com>
2020-10-02 14:33:24 -0500

    Initial implementation of Event routes

--- controllers/eventController.js
+++ controllers/eventController.js
@@ -1,5 +1,7 @@
 const Event = require('../models/events');
-const { body } = require('express-validator');
+const { body, validationResult } = require('express-validator');
+const { NotExtended } = require('http-errors');
+const { isDate } = require('moment');
 
 // Calender Page - Event List
 exports.calender = (req, res) => {
@@ -7,36 +9,134 @@ exports.calender = (req, res) => {
 };
 
 // Display detail page of specific event
-exports.event_detail = (req, res) => {
-    res.send(`NOT IMPLEMENTED: Event detail: ${req.params.id}`);
+exports.event_detail = (req, res, next) => {
+    Event.findById(req.params.id)
+        .exec(function(err, event) {
+            if (err) {return next(err); }
+            if (event === null) {
+                const err = new Error('Event does not exist');
+                err.status = 404;
+                return next(err);
+            }
+            // Successful, render
+            res.redirect('event_detail', { title: 'Event Details', event: event });
+        })
 };
 
 // Display Event Create Form - GET
-exports.event_create_get = (req, res) => {
-    res.render('event_form', { title: 'New Event' });
+exports.event_create_get = (req, res, next) => {
+    res.render('event_form', { title: 'Create New Event' });
 };
 
 // Handle Event Create - POST
-exports.event_create_post = (req, res) => {
-    res.send(`NOT IMPLEMENTED: Event create POST`);
-};
+exports.event_create_post = [
+    // Validate & sanitize fields
+    body('description').notEmpty().trim().escape().isLength({ min: 2 }),
+    body('address').notEmpty().trim().escape(),
+    body('meeting-date').notEmpty(),
+    body('start-time').notEmpty().escape(),
+    body('end-time').notEmpty().escape(),
+    // Process after validation
+    (req, res, next) => {
+        const errors = validationResult(req);
+        // Create New Event Object
+        const event = new Event({
+            description: req.body.description,
+            address: req.body.address,
+            meeting_date: req.body.meeting_date,
+            meeting_start_time: req.body.meeting_start_time,
+            meeting_end_time: req.body.meeting_end_time
+        });
+        if(!errors.isEmpty()){
+            // There are errors
+            res.render('event_form', { title: 'Create New Event', event: event, errors: errors.array() });
+            return;
+        } else {
+            // fields are valid! Ensure there are no duplicates
+            Event.findOne({'address': req.body.address})
+                .exec(function(err, found_event) {
+                    if (err) { return next(err); }
+                    if(found_event) {
+                        res.redirect(found_event.url);
+                    }
+                    else {
+                        event.save(function(err) {
+                            if (err) { return next(err); }
+                            res.redirect('event_detail', { title: 'Event Detail', event: event });
+                        });
+                    }
+                });
+        }
+    }
+];
 
 // Display Event Delete - GET
-exports.event_delete_get = (req, res) => {
-    res.send('NOT IMPLEMENTED: Event delete GET');
+exports.event_delete_get = (req, res, next) => {
+    Event.findById(req.params.id)
+        .exec(function(err, event) {
+            if(err) { return next(err); }
+            if(event === null) {
+                res.redirect('/calender');
+            }
+            // Successful
+            res.render('event_delete', { title: 'Delete Event', event: event });
+        });
 };
 
 // Handle Event Delete - POST
-exports.event_delete_post = (req, res) => {
-    res.send('NOT IMPLEMENTED: Event delete POST');
+exports.event_delete_post = (req, res, next) => {
+    Event.findByIdAndRemove(req.body.id, function deleteEvent(err) {
+        if (err) { return next(err); }
+        // Success
+        res.redirect('/calender');
+    })
 };
 
 // Display Event Update - GET
 exports.event_update_get = (req, res) => {
-    res.send('NOT IMPLEMENTED: Event update GET');
+    Event.findById(req.params.id, function(err, event) {
+        if (err) { return next(err); }
+        if (event === null) {
+            const err = new Error('Event not Found');
+            err.status = 404;
+            return next(err);
+        }
+        // Success
+        res.render('event_form', { title: 'Update Event', event: event });
+    })
 };
 
 // Handle Event Update - POST
-exports.event_update_post = (req, res) => {
-    res.send('NOT IMPLEMENTED: Event update POST');
-};
+exports.event_update_post = [
+     // Validate & sanitize fields
+     body('description').notEmpty().trim().escape().isLength({ min: 2 }),
+     body('address').notEmpty().trim().escape(),
+     body('meeting-date').notEmpty(),
+     body('start-time').notEmpty().escape(),
+     body('end-time').notEmpty().escape(),
+     // Process after validation
+     (req, res, next) => {
+         const errors = validationResult(req);
+         // Create Event object
+         const event = new Event({
+             description: req.body.description,
+             address: req.body.address,
+             meeting_date: req.body.meeting_date,
+             meeting_start_time: req.body.meeting_start_time,
+             meeting_end_time: req.body.meeting_end_time,
+             _id: req.params.id
+         });
+         if(!errors.isEmpty()){
+             // Errors, render form again and fix
+             res.render('event_form', { title: 'Update Event', event: event, errors: errors.array() });
+             return;
+         } else {
+             // Valid, find item by id and update
+             Event.findByIdAndUpdate(req.params.id, event, {}, function(err, theEvent) {
+                if(err) { return next(err); }
+                // Successful, redirect to detail page
+                res.redirect(theEvent.url);
+             });
+         }
+     }
+];
