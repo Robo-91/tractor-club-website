8bf69ce91139fe280f5765b3749fec316baf7793
Robert Palmer <r91palmer@gmail.com>
2020-10-20 14:44:21 -0500

    add ensureauthenticated middleware to form routes

--- routes/index.js
+++ routes/index.js
@@ -16,19 +16,19 @@ router.get('/', app_controller.index);
 // EVENT ROUTES //
 
 // Get create an Event
-router.get('/calender/create', event_controller.event_create_get);
+router.get('/calender/create', ensureAuthenticated, event_controller.event_create_get);
 
 // POST create an Event
 router.post('/calender/create', event_controller.event_create_post);
 
 // Get delete Event
-router.get('/calender/:id/delete', event_controller.event_delete_get);
+router.get('/calender/:id/delete', ensureAuthenticated, event_controller.event_delete_get);
 
 // POST delete Event
 router.post('/calender/:id/delete', event_controller.event_delete_post);
 
 // Get update Event
-router.get('/calender/:id/update', event_controller.event_update_get);
+router.get('/calender/:id/update', ensureAuthenticated, event_controller.event_update_get);
 
 // POST update Event
 router.post('/calender/:id/update', event_controller.event_update_post);
@@ -42,13 +42,13 @@ router.get('/calender', event_controller.calender);
 // PHOTO GALLERY ROUTES //
 
 // Get create Album
-router.get('/album/create', album_controller.album_create_get);
+router.get('/album/create', ensureAuthenticated, album_controller.album_create_get);
 
 // POST create Album
 router.post('/album/create', album_controller.album_create_post);
 
 // Get delete Album
-router.get('/album/:id/delete', album_controller.album_delete_get);
+router.get('/album/:id/delete', ensureAuthenticated, album_controller.album_delete_get);
 
 // POST delete Album
 router.post('/album/:id/delete', album_controller.album_delete_post);
@@ -62,13 +62,13 @@ router.get('/photogallery', album_controller.photo_gallery);
 // PHOTO ROUTES
 
 // Get create Photo
-router.get('/photo/create', photo_controller.photo_create_get);
+router.get('/photo/create', ensureAuthenticated, photo_controller.photo_create_get);
 
 // POST create Photo
 router.post('/photo/create', photo_controller.photo_create_post);
 
 // Get Delete Photo
-router.get('/photo/:id/delete', photo_controller.photo_delete_get);
+router.get('/photo/:id/delete', ensureAuthenticated, photo_controller.photo_delete_get);
 
 // POST Delete Photo
 router.post('/photo/:id/delete', photo_controller.photo_delete_post);
