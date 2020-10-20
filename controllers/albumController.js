9c7a2078fdc22c494aa7b98497cfeff794ad0fed
Robert Palmer <r91palmer@gmail.com>
2020-10-20 14:41:21 -0500

    Omitted multer logic from album route

--- controllers/albumController.js
+++ controllers/albumController.js
@@ -1,40 +1,6 @@
 const Album = require('../models/album');
 const Photo = require('../models/photos');
 const { body, validationResult } = require('express-validator');
-const multer = require('multer');
-const path = require('path');
-const fs = require('fs');
-
-// Set storage - Multer
-const storage = multer.diskStorage({
-    destination: './public/images/',
-    filename: function(req, file, cb) {
-        cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname));
-    }
-});
-
-function checkFileType(file, cb) {
-    // allowed extensions
-    const filetypes = /jpeg|jpg|png|gif/;
-    // check ext
-    const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
-    // check mime
-    const mimetype = filetypes.test(file.mimetype);
-
-    if (mimetype && extname) {
-        return cb(null, true);
-    } else {
-        cb('Error: Images Only!');
-    }
-}
-
-const upload = multer({
-    storage: storage,
-    limits: { fileSize: 1000000 },
-    fileFilter: function (req, file, cb) {
-        checkFileType(file, cb);
-    }
-});
 
 // Gallery of Photos
 exports.photo_gallery = (req, res, next) => {
